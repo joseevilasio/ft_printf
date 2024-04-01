6a6ef42c75224f451aa88f8e991a2e83ea6aac5e
joseevilasio <jose_evilasio@outlook.com>
2024-04-01 11:14:37 +0100

    fts bonus

--- src/bonus/ft_printf_bonus.h
+++ src/bonus/ft_printf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: josejunior <josejunior@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 16:11:37 by josejunior        #+#    #+#             */
-/*   Updated: 2024/03/25 22:09:28 by josejunior       ###   ########.fr       */
+/*   Updated: 2024/04/01 00:14:38 by josejunior       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,21 @@ typedef enum t_bool { FALSE, TRUE }	t_bool;
 typedef struct t_listflag
 {
 	char	flag;
-	t_bool	execute;
+	t_bool	exe;
 
 }	t_listflag;
 
-int	ft_parserflag(char *str, va_list args, t_listflag **l_flags, char **n);
+int	ft_parserflag(char *str, va_list args, t_listflag **lflags, char **n);
 int	ft_length_flags(char *str);
-int	ft_putnchar_bonus(int c, int n);
-int	ft_format_bonus(va_list args, const char fmt, t_listflag *l_flags, int nbr);
 int	ft_printf(const char *str, ...);
-int	ft_length_nbrhexa(unsigned long long n);
-int	ft_putstr_bonus(char *str, char flag, int n);
+int	ft_isflag(char flag, t_listflag *lflags);
+/* Refactory fts for bonus */
+int	ft_putnchar_bonus(int c, int n);
+int	ft_putstr_bonus(char *str, t_listflag *lflags, int n);
+int	ft_putchar_bonus(int c, t_listflag *lflags, int n);
+int	ft_putnbr_bonus(int n, t_listflag *lflags, int nbr);
+int	ft_putpointer_bonus(unsigned long long n, t_listflag *lflags, int nbr);
+int	ft_putnbrhex_bonus(unsigned int n, char fmt, t_listflag *lflags, int nbr);
+int	ft_putunsignednbr_bonus(unsigned int n, t_listflag *lflags, int nbr);
 
 #endif //FT_PRINTF_BONUS_H
---
+++ src/bonus/ft_putunsignednbr_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putunsignednbr_bonus.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josejunior <josejunior@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 18:39:25 by josejunior        #+#    #+#             */
+/*   Updated: 2024/04/01 11:13:44 by josejunior       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf_bonus.h"
+
+static int	intlen_bonus(unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	if (n > 9)
+	{
+		i += intlen_bonus(n / 10);
+		i++;
+	}
+	else
+		i++;
+	return (i);
+}
+
+static int	putunsignednbr_bonus(unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	if (n > 9)
+	{
+		i += putunsignednbr_bonus(n / 10);
+		i += ft_putchar(n % 10 + '0');
+	}
+	else
+		i += ft_putchar(n % 10 + '0');
+	return (i);
+}
+
+int	ft_putunsignednbr_bonus(unsigned int n, t_listflag *lflags, int nbr)
+{
+	int	i;
+
+	i = 0;
+	i = 0;
+	if (ft_isflag('-', lflags) == 1)
+	{
+		i += putunsignednbr_bonus(n);
+		if (ft_isflag('-', lflags) == 1)
+		i += ft_putnchar_bonus(' ', nbr - i);
+	}
+	else
+	{
+		if (ft_isflag('0', lflags) == 1 || ft_isflag('.', lflags) == 1)
+			i += ft_putnchar_bonus('0', nbr - intlen_bonus(n));
+		else if (ft_isflag('n', lflags) == 1)
+			i += ft_putnchar_bonus(' ', nbr - intlen_bonus(n));
+		i += putunsignednbr_bonus(n);
+	}
+	return (i);
+}
